4dbf132614c2b4a8a79b0e580053ee960cfcd880
Shirin <g00363458@gmit.ie>
2019-10-07 13:52:17 +0100

    first commit

--- src/Components/create.js
+++ src/Components/create.js
@@ -3,10 +3,68 @@ import React from 'react';
 
 
 class Create extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {Title: '', Year: '', Poster: ''};
+
+    this.handleChangeMovieTitle = this.handleChangeMovieTitle.bind(this);
+    this.handleChangeMovieYear = this.handleChangeMovieYear.bind(this);
+    this.handleChangeMoviePoster = this.handleChangeMoviePoster.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeMovieTitle(e) {
+    this.setState({Title: e.target.value});
+  }
+  handleChangeMovieYear(e) {
+    this.setState({Year: e.target.value});
+  }
+
+  handleChangeMoviePoster(e) {
+    this.setState({Poster: e.target.value});
+  }
+
+  handleSubmit(e) {
+    alert('A name was submitted: ' + this.state.Title +''+ 'Year:'+ this.state.Year + ''+'Poster:'+ this.state.Poster);
+    e.preventDefault();
+      }
+
   render(){
   return (
     <div>
      <h1>Hello from another component</h1>
+     <form onSubmit={this.handleSubmit}>
+       <div className="form-group">
+        <label>
+          Movie Title:
+          </label>
+          <input type="text" value={this.state.Title} onChange={this.handleChangeMovieTitle} />
+        
+        
+
+      
+        </div>
+        <div className="form-group">
+        <label>
+        Movie Year:
+          </label>
+          <input type="text" value={this.state.Year} onChange={this.handleChangeMovieYear} />
+        
+        
+        </div>
+
+        <div className="form-group">
+        <label>
+          Movie Poster Url
+        </label>
+        <textarea className='form-control' rows = '6' value ={this.state.Poster} onChange={this.handleChangeMoviePoster} ></textarea>
+        <div className='form-group'>
+        <input type="submit" value="Submit" />
+        </div>
+        </div>
+      </form>
+      
      
     </div>
   );
